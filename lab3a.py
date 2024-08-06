310256becf535286bf6e35e3c1df9dc77022eb5f
Sambhav Sachdeva <ssachdeva25@myseneca.ca>
2024-08-05 20:25:14 -0400

    lab3a.py

---
+++ lab3a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+
+# Author ID: ssachdeva25
+
+def return_text_value():
+    return "Hello, this is a greeting!"
+
+
+
+def return_number_value():
+    return 42
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
